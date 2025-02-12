d1e5806a8c35b0ee87f4414cba91cb3e2659afe5
Arun Shekar <shekar.arun@gmail.com>
2025-02-12 23:22:29 +0530

    feat: Add readme upon repo init

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -4,3 +4,10 @@ resource "github_repository" "auto-repo" {
   visibility  = "private"
   auto_init   = true
 }
+
+resource "github_repository_file" "readme" {
+  repository          = github_repository.auto-repo.name
+  file                = "README.md"
+  content             = "This is an auto created repo through Terraform"
+  overwrite_on_create = true
+}
