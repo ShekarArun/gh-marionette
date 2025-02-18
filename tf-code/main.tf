6c7d0510ace04b57b96bfd6bf6b821fd687777bf
Arun Shekar <shekar.arun@gmail.com>
2025-02-18 20:15:41 +0530

    feat: Add count to create multiple resources

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -1,23 +1,27 @@
 resource "random_id" "random" {
   byte_length = 2
+  count       = 2
 }
 
 resource "github_repository" "auto-repo" {
-  name        = "tf-autocreate-${random_id.random.dec}"
+  count       = 2
+  name        = "tf-autocreate-${random_id.random[count.index].dec}"
   description = "Auto created repo through Terraform"
   visibility  = "private"
   auto_init   = true
 }
 
 resource "github_repository_file" "readme" {
-  repository          = github_repository.auto-repo.name
+  count               = 2
+  repository          = github_repository.auto-repo[count.index].name
   file                = "README.md"
   content             = "This is an auto created repo through Terraform"
   overwrite_on_create = true
 }
 
 resource "github_repository_file" "index" {
-  repository          = github_repository.auto-repo.name
+  count               = 2
+  repository          = github_repository.auto-repo[count.index].name
   file                = "index.html"
   content             = "This is an HTML file!"
   overwrite_on_create = true
