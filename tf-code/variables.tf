4ed04eea800dce75143cd25fd9e586cad944a4b0
Arun Shekar <shekar.arun@gmail.com>
2025-02-20 14:30:53 +0530

    chore: Add var for environment config

--- tf-code/variables.tf
+++ tf-code/variables.tf
@@ -2,6 +2,21 @@ variable "repo_count" {
   type        = number
   description = "Number of copies of resources"
   default     = 1
+
+  validation {
+    condition     = var.repo_count < 5
+    error_message = "More than 4 repositories not allowed"
+  }
+}
+
+variable "env" {
+  type        = string
+  description = "Deployment Environment"
+
+  validation {
+    condition     = var.env == "dev" || var.env == "prd"
+    error_message = "Environment can only be 'dev' or 'prd'"
+  }
 }
 
 # variable "varsource" {
