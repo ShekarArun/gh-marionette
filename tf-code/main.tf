f79b2d13bf44e22a83e2f10856546ad20e372ee7
Arun Shekar <shekar.arun@gmail.com>
2025-02-26 18:03:00 +0530

    chore: Switch to using list of env

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -1,34 +1,40 @@
-resource "random_id" "random" {
-  byte_length = 2
-  count       = var.repo_count
-}
+# resource "random_id" "random" {
+#   byte_length = 2
+#   count       = var.repo_count
+# }
 
 resource "github_repository" "auto-repo" {
-  count       = var.repo_count
-  name        = "tf-autocreate-${random_id.random[count.index].dec}"
-  description = "Auto created repo through Terraform"
-  visibility  = var.env == "prd" ? "public" : "private"
-  auto_init   = true
+  for_each    = toset(["dev", "prd"])
+  name        = "auto-repo-${each.key}"
+  description = "${each.value} repo for auto init"
+  # count       = var.repo_count
+  # name        = "tf-autocreate-${random_id.random[count.index].dec}"
+  # description = "Auto created repo through Terraform"
+  visibility = var.env == "prd" ? "public" : "private"
+  auto_init  = true
 }
 
-resource "github_repository_file" "readme" {
-  count               = var.repo_count
-  repository          = github_repository.auto-repo[count.index].name
-  file                = "README.md"
-  content             = "This is an auto created repo through Terraform, stage: ${var.env}"
-  overwrite_on_create = true
-}
+# resource "github_repository_file" "readme" {
+#   count               = var.repo_count
+#   repository          = github_repository.auto-repo[count.index].name
+#   file                = "README.md"
+#   content             = "This is an auto created repo through Terraform, stage: ${var.env}"
+#   overwrite_on_create = true
+# }
 
-resource "github_repository_file" "index" {
-  count               = var.repo_count
-  repository          = github_repository.auto-repo[count.index].name
-  file                = "index.html"
-  content             = "This is an HTML file!"
-  overwrite_on_create = true
-}
+# resource "github_repository_file" "index" {
+#   count               = var.repo_count
+#   repository          = github_repository.auto-repo[count.index].name
+#   file                = "index.html"
+#   content             = "This is an HTML file!"
+#   overwrite_on_create = true
+# }
 
 output "clone-urls" {
-  value       = { for i in github_repository.auto-repo[*] : i.name => i.ssh_clone_url }
+  value = {
+    for k, v in github_repository.auto-repo : k => v.ssh_clone_url
+  }
+  # value       = { for i in github_repository.auto-repo[*] : i.name => i.ssh_clone_url }
   description = "Repo names and URLs"
   sensitive   = false
 }
