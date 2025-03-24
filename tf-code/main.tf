37bf7e4703e5602428af7333c2c2ee9102197367
Arun Shekar <shekar.arun@gmail.com>
2025-03-24 18:32:24 +0530

    feat: Add null resource to delay cloning of repo through provisioner

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -13,16 +13,24 @@ resource "github_repository" "auto-repo" {
     command = "gh repo view ${self.name} --web"
   }
   provisioner "local-exec" {
-    command = "gh repo clone ${self.name}"
-    when    = create
-  }
-  provisioner "local-exec" {
     command = "rm -rf ${self.name}"
     when    = destroy
   }
   # count       = var.repo_count
   # name        = "tf-autocreate-${random_id.random[count.index].dec}"
   # description = "Auto created repo through Terraform"
+}
+
+resource "terraform_data" "repo-clone-provisioner-host" {
+  for_each = var.repos
+  depends_on = [
+    github_repository_file.index,
+    github_repository_file.readme
+  ]
+  provisioner "local-exec" {
+    command = "gh repo clone ${github_repository.auto-repo[each.key].name}"
+    when    = create
+  }
 }
 
 resource "github_repository_file" "readme" {
