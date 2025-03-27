967cc0b2fab76120d809b987aea2e17b94ba5605
Arun Shekar <shekar.arun@gmail.com>
2025-03-27 14:30:10 +0530

    build: Output GH Pages URL

--- tf-code/outputs.tf
+++ tf-code/outputs.tf
@@ -1,6 +1,9 @@
 output "clone-urls" {
   value = {
-    for k, v in github_repository.auto-repo : k => v.ssh_clone_url
+    for k, v in github_repository.auto-repo : k => {
+      ssh_url   = v.ssh_clone_url
+      pages_url = v.pages[0].html_url
+    }
   }
   # value       = { for i in github_repository.auto-repo[*] : i.name => i.ssh_clone_url }
   description = "Repo names and URLs"
